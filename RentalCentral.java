4681eb0c57cc7d8dabfe1fa29cbf965ae4e342cc
Jennifer Rose <146021903+niffinator@users.noreply.github.com>
2023-12-09 20:32:42 -0500

    Update RentalCentral

--- RentalCentral.java
+++ RentalCentral.java
@@ -15,6 +15,7 @@ import javafx.scene.layout.StackPane;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
+import java.lang.*;
 
 public class RentalCentral extends Application {
 
@@ -61,31 +62,89 @@ public class RentalCentral extends Application {
         // comment out the launch function when testing without GUI
         //launch();
         
-        // comment this out when using GUI        
+        // When using GUI - comment out the code that prints to the concole        
         
-        ArrayList<Customer> customerAccountsArray = new ArrayList<>();
-        ArrayList<Game> gameInventoryArray = new ArrayList<>();
-
-        Customer cust1, cust2, cust3;
-        cust1 = new Customer("Smil6575", "Samantha",
-            "Smiley", "123 Four Place", "Hometown", "IN",
-            "47409", 0.0);
-        customerAccountsArray.add(cust1);
-        cust2 = new Customer("Bail2345", "Trisha", "Bailey",
-            "234 South St", "Titusville", "FL", "32780",
-            0.0);
-        customerAccountsArray.add(cust2);
-        cust3 = new Customer("Prin6567", "Pebbles", "Prince",
-            "1999 Nikki Way", "Warminster", "PA", "18974",
-            0.0);
-        customerAccountsArray.add(cust3); 
+        // Create the customer and game lists associated with the rentals
+        ArrayList<Customer> customerList = new ArrayList<>();
+        ArrayList<Game> gameList = new ArrayList<>();
+                
+        // Create baseline 'beginning' Customer objects
+        Customer c1, c2, c3, c4, c5, c6;
+        c1 = new Customer("1", "John", "Doe", "123 Main St", "Springfield", "IL", "62701", 20.00);
+        c2 = new Customer("2", "Jane", "Smith", "456 Elm St", "Springfield", "IL", "62701", 30.00);
+        c3 = new Customer("3", "Jordan", "Williams", "952 Bark Pl", "Desmoine", "IA", "25906", 40.00);
+        c4 = new Customer("4", "Jayden", "Mendoza", "835 Rocket RD", "Seattle", "WA", "95708", 50.00);
+        c5 = new Customer("5", "Norwin", "Bermudez", "734 Angler CT", "Carmel", "IN", "46032", 60.00);
+        c6 = new Customer("6", "Angelita", "De La Cruz", "156 Whitestown Pkwy", "Zionsville", "IN", "46077", 70.00);
         
-        customerAccountsArray.add(createCustomer());
-        System.out.println(customerAccountsArray);
+        // Set unique customer Ids
+        c1.setCustomerId(c1.generateId(c1.getLastName()));
+        c2.setCustomerId(c2.generateId(c2.getLastName()));
+        c3.setCustomerId(c3.generateId(c3.getLastName()));
+        c4.setCustomerId(c4.generateId(c4.getLastName()));
+        c5.setCustomerId(c5.generateId(c5.getLastName()));
+        c6.setCustomerId(c6.generateId(c6.getLastName()));
         
-        gameInventoryArray.add(newGameToInventory());
-        System.out.println(gameInventoryArray);
-            
+        // Add baseline Customer objects to the Customer ArrayList
+        customerList.add(c1);
+        customerList.add(c2);
+        customerList.add(c3);
+        customerList.add(c4);
+        customerList.add(c5);
+        customerList.add(c6);
+        
+        // Add a customer using the createCustomer method
+        //customerList.add(createCustomer());
+                
+        // Create New Game Objects
+        Game g1, g2, g3, g4, g5, g6;
+        g1 = new Game("Rag n Bone", new Date(), "1", "Action", "E", "PC", "Available");
+        g2 = new Game("Link: WildBreath", new Date(), "2", "Adventure", "E", "Switch", "Available");
+        g3 = new Game("Call of Duty Warzone Warfare", new Date(), "3", "FPS", "M", "PlayStation", "Available");
+        g4 = new Game("Spongebob Bubble Brawl", new Date(), "4", "Action", "E", "PC", "Available");
+        g5 = new Game("Grilling Daddy", new Date(), "5", "Cooking Simulator", "E", "Xbox", "Available");
+        g6 = new Game("Zelda Tears of the Kingdom", new Date(), "6", "Adventure", "E", "Switch", "Available");
+        
+        // Set unique game Ids
+        g1.setGameId(g1.generateGameId(g1.getName(), g1.getPlatform()));
+        g2.setGameId(g2.generateGameId(g2.getName(), g2.getPlatform()));
+        g3.setGameId(g3.generateGameId(g3.getName(), g3.getPlatform()));
+        g4.setGameId(g4.generateGameId(g4.getName(), g4.getPlatform()));
+        g5.setGameId(g5.generateGameId(g5.getName(), g5.getPlatform()));
+        g6.setGameId(g6.generateGameId(g6.getName(), g6.getPlatform()));
+        
+        // Add the Game objects to the Game inventory
+        gameList.add(g1);
+        gameList.add(g2);
+        gameList.add(g3);
+        gameList.add(g4);
+        gameList.add(g5);
+        gameList.add(g6);
+        
+        // Add a game using the createGame method
+        //gameList.add(newGameToInventory());
+        
+        // Print the lists
+        printCustomerList(customerList);
+        printGameList(gameList);        
+    }
+    
+    // Call this method to print the customer list at any time
+    public static void printCustomerList(ArrayList<Customer> customers){
+        System.out.println("\nLIST OF CURRENT CUSTOMERS:");
+        for (Customer customer : customers) {
+            System.out.print("---------------");
+            System.out.print(customer.toString());
+        }
+    }
+        
+    // Call this method to print the game list at any time
+    public static void printGameList(ArrayList<Game> games){
+        System.out.println("\nLIST OF GAME INVENTORY:");
+        for (Game game : games) {
+            System.out.print("---------------");
+            System.out.print(game.toString());            
+        }
     }
     
     // Need to have ID, firstName, lastName, address, city, state, zip, & beginning balance
@@ -129,7 +188,7 @@ public class RentalCentral extends Application {
         game.setDateReceived(new java.util.Date());
         return game;
     }
-
+    
     // Search Methods
     public void searchByGameId(){
     }
